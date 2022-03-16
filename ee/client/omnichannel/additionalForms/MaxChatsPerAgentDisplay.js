afc38ea82246171a1fdf36876d62672916700a77
Laleet Avaiya <laleet.avaiya@cloudera.com>
2022-03-16 13:52:47 +0530

    first commit

---
+++ ee/client/omnichannel/additionalForms/MaxChatsPerAgentDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { useTranslation } from '../../../../client/contexts/TranslationContext';
+import UserInfo from '../../../../client/views/room/contextualBar/UserInfo';
+
+const MaxChatsPerAgentDisplay = ({ data: { livechat: { maxNumberSimultaneousChat = '' } = {} } = {} }) => {
+	const t = useTranslation();
+
+	return <>
+		<UserInfo.Label>{t('Max_number_of_chats_per_agent')}</UserInfo.Label>
+		<UserInfo.Info>{maxNumberSimultaneousChat}</UserInfo.Info>
+	</>;
+};
+
+export default MaxChatsPerAgentDisplay;
